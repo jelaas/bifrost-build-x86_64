8a6e852c14b7e45a29680c341b8003bef009644f
Jens Låås <jens@laas.mine.nu>
2011-03-19 08:46:41 +0100

    forgot DST

--- all/console-tools-lib-0.2.3-1/Fetch-source.sh
+++ all/console-tools-lib-0.2.3-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=console-tools-0.2.3.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST ftp://metalab.unc.edu/pub/Linux/system/keyboards/$SRC || wget -O http://ftp.sunet.se/pub/Linux/distributions/sourcemage/mirror/$SRC
+[ -s "$DST" ] || wget -O $DST ftp://metalab.unc.edu/pub/Linux/system/keyboards/$SRC || wget -O $DST http://ftp.sunet.se/pub/Linux/distributions/sourcemage/mirror/$SRC
