6cb7d81d56495a38e46f6aa2121c1e2930b5e072
Jens Låås <jens@jelaas.eu>
2016-03-02 16:27:04 +0100

    old sources

--- all/musl-openssl-1.0.2d-1/Fetch-source.sh
+++ all/musl-openssl-1.0.2d-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=openssl-1.0.2d.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://www.openssl.org/source/$SRC || wget -O $DST http://www.openssl.org/source/old/$SRC
+[ -s "$DST" ] || wget -O $DST http://www.openssl.org/source/$SRC || wget -O $DST http://www.openssl.org/source/old/1.0.2/$SRC
