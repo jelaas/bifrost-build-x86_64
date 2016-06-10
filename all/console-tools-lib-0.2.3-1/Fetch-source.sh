17afc106bfc887c4063d1f13a2df37602901c897
Jens Låås <jens@jelaas.eu>
2016-06-10 18:53:46 +0200

    change source location

--- all/console-tools-lib-0.2.3-1/Fetch-source.sh
+++ all/console-tools-lib-0.2.3-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=console-tools-0.2.3.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST ftp://metalab.unc.edu/pub/Linux/system/keyboards/$SRC || wget -O $DST http://sunsite.icm.edu.pl/pub/Linux/distributions/sourcemage/mirror/$SRC
+[ -s "$DST" ] || wget -O $DST http://www.ibiblio.org/pub/Linux/system/keyboards/$SRC
