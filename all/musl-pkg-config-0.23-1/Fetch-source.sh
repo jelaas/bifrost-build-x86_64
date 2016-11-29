29f2f3eb6c2112808018d47e56789ba955fb3ab8
Jens Låås <jens@jelaas.eu>
2016-11-29 15:09:00 +0100

    fix source

--- all/musl-pkg-config-0.23-1/Fetch-source.sh
+++ all/musl-pkg-config-0.23-1/Fetch-source.sh
@@ -3,4 +3,5 @@
 SRC=pkg-config-0.23.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://pkg-config.freedesktop.org/releases/$SRC
+pkg_install curl-7.51.0-1 || exit 2
+[ -s "$DST" ] || curl -L -k -o $DST http://pkg-config.freedesktop.org/releases/$SRC
