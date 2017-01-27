412da8aafc6a5ff7055af2add7c3c54b88966046
Jens Låås <jens@jelaas.eu>
2017-01-27 11:41:52 +0100

    source location

--- all/openssl-0.9.8x-1/Fetch-source.sh
+++ all/openssl-0.9.8x-1/Fetch-source.sh
@@ -3,4 +3,4 @@
 SRC=openssl-0.9.8x.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://www.openssl.org/source/$SRC
+[ -s "$DST" ] || wget -O $DST http://www.openssl.org/source/$SRC || wget -O $DST https://www.openssl.org/source/old/0.9.x/$SRC
