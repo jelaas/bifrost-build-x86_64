9ae316087ef16db4eff4c5a3774f9b2012d1090d
Jens Låås <jens@jelaas.eu>
2017-02-02 15:43:22 +0100

    fix source location

--- all/musl-pcre-8.30-2/Fetch-source.sh
+++ all/musl-pcre-8.30-2/Fetch-source.sh
@@ -2,5 +2,9 @@
 
 SRC=pcre-8.30.tar.bz2
 DST=/var/spool/src/$SRC
+SHA=183e40bca3c170611ba46205c950b9ed8385f9c832086e042254e016b04e5db6
 
-[ -s "$DST" ] || wget -O $DST ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/$SRC || wget -O $DST http://kent.dl.sourceforge.net/project/pcre/pcre/8.30/$SRC
+pkg_install tarmd-1.2-1 || exit 2
+pkg_install curl-7.51.0-1 || exit 2
+
+[ -s "$DST" ] || tarmd $SHA $DST curl -L -k  http://downloads.sourceforge.net/project/pcre/pcre/8.30/$SRC
