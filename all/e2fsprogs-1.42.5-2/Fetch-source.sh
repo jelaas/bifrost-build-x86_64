00da168e9bf392ab5c7b0d346b4712887ac79e0e
Jens Låås <jens@jelaas.eu>
2017-02-15 16:05:18 +0100

    https fetch

--- all/e2fsprogs-1.42.5-2/Fetch-source.sh
+++ all/e2fsprogs-1.42.5-2/Fetch-source.sh
@@ -3,4 +3,5 @@
 SRC=e2fsprogs-1.42.5.tar.gz
 DST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://downloads.sourceforge.net/project/e2fsprogs/e2fsprogs/v1.42.5/$SRC
+pkg_install curl-7.51.0-1 || exit 2
+[ -s "$DST" ] || curl -L -k -o $DST http://downloads.sourceforge.net/project/e2fsprogs/e2fsprogs/v1.42.5/$SRC
