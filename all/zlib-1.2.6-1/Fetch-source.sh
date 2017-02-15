647b7f5a675286fa7f976545454b91339b3bcb53
Jens Låås <jens@jelaas.eu>
2017-02-15 14:28:46 +0100

    source location

--- all/zlib-1.2.6-1/Fetch-source.sh
+++ all/zlib-1.2.6-1/Fetch-source.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 SRC=zlib-1.2.6.tar.bz2
-DST=/var/spool/src/$SRC
+DST=/tmp/$SRC
+VDST=/var/spool/src/$SRC
 
-[ -s "$DST" ] || wget -O $DST http://downloads.sourceforge.net/project/libpng/zlib/1.2.6/$SRC || http://www.zlib.net/$SRC || wget -O $DST ftp://ftp.simplesystems.org/pub/libpng/png/src/history/$SRC
+pkg_install curl-7.51.0-1 || exit 2
+if ! [ -s "$VDST" ]; then
+    pkg_install tarmd-nocomp-1.2-1 || exit 2
+    curl -L -k -o $DST https://downloads.sourceforge.net/project/libpng/zlib/1.2.6/$SRC || exit 1
+    bzcat "$DST" | tarmd fc4034156d1b890eaba4a2bf73a1d4b2b2d22807362eccfe2b6697f1b9a9bb39 $VDST
+fi
